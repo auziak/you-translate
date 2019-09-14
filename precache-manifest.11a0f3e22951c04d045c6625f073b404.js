8e12c37128afc82148b369fa64afb45e64c00699
auziak <auziak90@gmail.com>
2019-09-14 13:47:28 +0300

    Updates

---
+++ precache-manifest.11a0f3e22951c04d045c6625f073b404.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6824927e4d33bc95e04ab8b0fa40cdb7",
+    "url": "/auziak/you-translate/index.html"
+  },
+  {
+    "revision": "2df7327c3696401f8a23",
+    "url": "/auziak/you-translate/static/css/main.2ff4325a.chunk.css"
+  },
+  {
+    "revision": "493a8d9941958b915592",
+    "url": "/auziak/you-translate/static/js/2.9294c85b.chunk.js"
+  },
+  {
+    "revision": "2df7327c3696401f8a23",
+    "url": "/auziak/you-translate/static/js/main.3eedd156.chunk.js"
+  },
+  {
+    "revision": "11604f7038d4c90413e4",
+    "url": "/auziak/you-translate/static/js/runtime~main.eca5f771.js"
+  }
+]);
