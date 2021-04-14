69362b865100a147299bd8e13057b2a9aa400f83
Druce Vsemohutnii <auziak90@gmail.com>
2021-04-14 21:24:19 +0300

    Updates

--- precache-manifest.b3119cf04cf0924305fbbdf52d08b8d6.js
+++ precache-manifest.b3119cf04cf0924305fbbdf52d08b8d6.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "8c8266fa79eedf3d114984d154647de1",
+    "revision": "cdcf31dd8957204ed397ad4a5c27f064",
     "url": "/you-translate/index.html"
   },
   {
-    "revision": "5461c15dbd9882e750b2",
-    "url": "/you-translate/static/css/main.73e32845.chunk.css"
+    "revision": "282e85daf20108327b88",
+    "url": "/you-translate/static/css/main.3c9662f9.chunk.css"
   },
   {
     "revision": "08c9e760953a4000f620",
     "url": "/you-translate/static/js/2.7ccc68e9.chunk.js"
   },
   {
-    "revision": "5461c15dbd9882e750b2",
-    "url": "/you-translate/static/js/main.d4e49325.chunk.js"
+    "revision": "282e85daf20108327b88",
+    "url": "/you-translate/static/js/main.a62622f1.chunk.js"
   },
   {
     "revision": "7b8422d8f46be50d8d60",
